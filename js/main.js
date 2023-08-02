abe8821329703e1d8377ee2039b58d198202781b
DrusJs <magnojoky962@gmail.com>
2023-08-02 18:49:46 +0300

    Remove animation classes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 document.getElementById("burger-button").addEventListener("click", (el)=>{
     el.currentTarget.classList.toggle("active");
 });
+setTimeout(()=>{
+    try {
+        document.querySelector(".animone").classList.remove("animone");    
+        document.querySelector(".animtwo").classList.remove("animtwo");    
+        document.querySelector(".animthree").classList.remove("animthree");
+    } catch {}
+}, 1300)
