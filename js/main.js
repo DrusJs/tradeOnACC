9d8f5d9f6d433921bdc3cdad0987931eaaf47862
DrusJs <magnojoky962@gmail.com>
2023-08-02 22:44:30 +0300

    Add store page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 document.getElementById("burger-button").addEventListener("click", (el)=>{
     el.currentTarget.classList.toggle("active");
 });
+
 setTimeout(()=>{
     try {
         document.querySelector(".animone").classList.remove("animone");    
         document.querySelector(".animtwo").classList.remove("animtwo");    
         document.querySelector(".animthree").classList.remove("animthree");
     } catch {}
-}, 1300)
+}, 1300);
+
+try {
+    var accountTotal = document.querySelector(".buy-input .total");
+    setInputNumberAction()
+} catch {}
+
+function setInputNumberAction() {
+    accountTotal.previousElementSibling.addEventListener("click", ()=>{
+        accountTotal.innerHTML = accountTotal.innerHTML=="0"?0:+accountTotal.innerHTML-50;
+    });
+    accountTotal.nextElementSibling.addEventListener("click", ()=>{
+        accountTotal.innerHTML = accountTotal.innerHTML=="950"?950:+accountTotal.innerHTML+50;
+    });
+}
