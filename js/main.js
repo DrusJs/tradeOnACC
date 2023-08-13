6fd74431fa2fb955b412d4c6b36c8bd1504c5bf6
DrusJs <magnojoky962@gmail.com>
2023-08-13 23:30:28 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /////////////////////
 ////////burger///////
 /////////////////////
@@ -21,91 +20,70 @@ burgerButton.addEventListener("click", ()=>{
 ////////modals///////
 /////////////////////
 if (document.querySelector(".modal-wrapper")) {
-const modalWrappers = document.querySelectorAll(".modal-wrapper");
-const modalCloseButtons = document.querySelectorAll(".modal-close");
-modalWrappers.forEach(el =>{
-    el.addEventListener("click", (e) => {
-        if (e.target.classList.contains("modal-wrapper")){
-            e.currentTarget.classList.remove("active");
-            document.body.classList.remove("noscroll");
-        }
+    const modalWrappers = document.querySelectorAll(".modal-wrapper");
+    const modalCloseButtons = document.querySelectorAll(".modal-close");
+    modalWrappers.forEach(el =>{
+        el.addEventListener("click", (e) => {
+            if (e.target.classList.contains("modal-wrapper")){
+                e.currentTarget.classList.remove("active");
+                document.body.classList.remove("noscroll");
+            }
+        });
     });
-});
 
-modalCloseButtons.forEach(el => {
-    el.addEventListener("click", (e) => {
-        e.currentTarget.parentElement.parentElement.classList.remove("active");
-        document.body.classList.remove("noscroll");
+    modalCloseButtons.forEach(el => {
+        el.addEventListener("click", (e) => {
+            e.currentTarget.parentElement.parentElement.classList.remove("active");
+            document.body.classList.remove("noscroll");
+        });
     });
-});
-//modal-accounts
-document.querySelector(".footer-logo").addEventListener("click", () => {
-    document.getElementById("modal-accounts").classList.add("active");
-    document.body.classList.add("noscroll");
-});
-//login-modal-open
-document.querySelector(".auth__sign-in").addEventListener("click", () => {
-    document.getElementById("modal-login").classList.add("active");
-    document.body.classList.add("noscroll");
-});
-document.querySelector(".logmodal-open").addEventListener("click", () => {
-    document.getElementById("modal-registration").classList.remove("active");
-    document.getElementById("modal-login").classList.add("active");
-});
-//registration-modal-open
-document.querySelector(".auth__sign-up").addEventListener("click", () => {
-    document.getElementById("modal-registration").classList.add("active");
-    document.body.classList.add("noscroll");
-});
-document.querySelector(".regmodal-open").addEventListener("click", () => {
-    document.getElementById("modal-login").classList.remove("active");
-    document.getElementById("modal-registration").classList.add("active");
-});
-//remind-modal 
-document.querySelector(".remind-pass").addEventListener("click", () => {
-    document.getElementById("modal-login").classList.remove("active");
-    document.getElementById("modal-remind").classList.add("active");
-});
-document.querySelectorAll(".return-button").forEach((el)=>{
-        el.addEventListener("click", () => {
-        document.getElementById("modal-remind").classList.remove("active");
-        document.getElementById("modal-reset").classList.remove("active");
+    //modal-accounts
+    document.querySelector(".footer-logo").addEventListener("click", () => {
+        document.getElementById("modal-accounts").classList.add("active");
+        document.body.classList.add("noscroll");
+    });
+    //login-modal-open
+    document.querySelector(".auth__sign-in").addEventListener("click", () => {
+        document.getElementById("modal-login").classList.add("active");
+        document.body.classList.add("noscroll");
+    });
+    document.querySelector(".logmodal-open").addEventListener("click", () => {
+        document.getElementById("modal-registration").classList.remove("active");
         document.getElementById("modal-login").classList.add("active");
     });
-});
+    //registration-modal-open
+    document.querySelector(".auth__sign-up").addEventListener("click", () => {
+        document.getElementById("modal-registration").classList.add("active");
+        document.body.classList.add("noscroll");
+    });
+    document.querySelector(".regmodal-open").addEventListener("click", () => {
+        document.getElementById("modal-login").classList.remove("active");
+        document.getElementById("modal-registration").classList.add("active");
+    });
+    //remind-modal 
+    document.querySelector(".remind-pass").addEventListener("click", () => {
+        document.getElementById("modal-login").classList.remove("active");
+        document.getElementById("modal-remind").classList.add("active");
+    });
+    document.querySelectorAll(".return-button").forEach((el)=>{
+            el.addEventListener("click", () => {
+            document.getElementById("modal-remind").classList.remove("active");
+            document.getElementById("modal-reset").classList.remove("active");
+            document.getElementById("modal-login").classList.add("active");
+        });
+    });
 
-document.querySelector(".none-balance-button").addEventListener("click", ()=> {
-    document.getElementById("modal-none-balance").classList.remove("active");
-    document.getElementById("modal-balance").classList.add("active");
-})
+    document.querySelector(".none-balance-button").addEventListener("click", ()=> {
+        document.getElementById("modal-none-balance").classList.remove("active");
+        document.getElementById("modal-balance").classList.add("active");
+    })
 
-let timerEmail;
-document.querySelector(".remind__password-modal .modal-button.check").addEventListener("click", () => {
-    // Если почта не найдена, то
-    // document.querySelector(".remind__password-modal").classList.add("error"); return;
-    document.querySelector(".remind__password-modal").classList.add("accept");
-    document.querySelector(".repeat-button").classList.add("await");
-    document.querySelector(".timeline-modal").innerHTML = "4:59";
-    let timeMinut = 298;
-    timerEmail = setInterval(function () {
-        seconds = timeMinut%60;
-        minuts = timeMinut/60%60;
-        if (timeMinut < 1) {
-            document.querySelector(".timeline-modal").innerHTML = "0:00";
-            clearInterval(timerEmail);
-            document.querySelector(".repeat-button").classList.remove("await");
-        } else {
-            seconds = seconds>9?seconds:"0"+seconds;
-            let strTimer = `${Math.trunc(minuts)}:${seconds}`;
-            document.querySelector(".timeline-modal").innerHTML = strTimer;
-        }
-        --timeMinut;
-    }, 1000)
-});
-document.querySelector(".repeat-button").addEventListener("click", (el) => {
-    if (document.querySelector(".timeline-modal").innerHTML == "0:00"){
+    let timerEmail;
+    document.querySelector(".remind__password-modal .modal-button.check").addEventListener("click", () => {
+        // Если почта не найдена, то
+        // document.querySelector(".remind__password-modal").classList.add("error"); return;
+        document.querySelector(".remind__password-modal").classList.add("accept");
         document.querySelector(".repeat-button").classList.add("await");
-        clearInterval(timerEmail);
         document.querySelector(".timeline-modal").innerHTML = "4:59";
         let timeMinut = 298;
         timerEmail = setInterval(function () {
@@ -122,10 +100,33 @@ document.querySelector(".repeat-button").addEventListener("click", (el) => {
             }
             --timeMinut;
         }, 1000)
-    }
-})
+    });
+    document.querySelector(".repeat-button").addEventListener("click", (el) => {
+        if (document.querySelector(".timeline-modal").innerHTML == "0:00"){
+            document.querySelector(".repeat-button").classList.add("await");
+            clearInterval(timerEmail);
+            document.querySelector(".timeline-modal").innerHTML = "4:59";
+            let timeMinut = 298;
+            timerEmail = setInterval(function () {
+                seconds = timeMinut%60;
+                minuts = timeMinut/60%60;
+                if (timeMinut < 1) {
+                    document.querySelector(".timeline-modal").innerHTML = "0:00";
+                    clearInterval(timerEmail);
+                    document.querySelector(".repeat-button").classList.remove("await");
+                } else {
+                    seconds = seconds>9?seconds:"0"+seconds;
+                    let strTimer = `${Math.trunc(minuts)}:${seconds}`;
+                    document.querySelector(".timeline-modal").innerHTML = strTimer;
+                }
+                --timeMinut;
+            }, 1000)
+        }
+    })
 }
-
+/////////////////////
+/////modals.END//////
+/////////////////////
 if (document.querySelector(".more-review")) {
     document.querySelector(".more-review").addEventListener("click", (el) => {
         el.currentTarget.style.display = "none";
